e5d54a3e58f45729394f047fb9b0e3d312e1f0f4
Yangkai.Shen <237497819@qq.com>
2019-09-18 15:30:02 +0800

    :tada: 初始化

---
+++ src/main/java/com/xkcoding/spelaopdemo/SpelAopDemoApplication.java
@@ -0,0 +1,21 @@
+package com.xkcoding.spelaopdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ * 启动器
+ * </p>
+ *
+ * @author yangkai.shen
+ * @date Created in 2019/9/18 11:37
+ */
+@SpringBootApplication
+public class SpelAopDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpelAopDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xkcoding/spelaopdemo/SpelAopDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xkcoding.spelaopdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpelAopDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
